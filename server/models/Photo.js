f97293b5c4aab592f2cfd27c262129dfbd88ffb6
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-10-23 22:26:49 -0400

    added user photo and reaction models

--- server/models/Photo.js
+++ server/models/Photo.js
@@ -2,14 +2,12 @@ const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
 
-const thoughtSchema = new Schema(
+const photoSchema = new Schema(
   {
-    thoughtText: {
-      type: String,
-      required: 'You need to leave a thought!',
-      minlength: 1,
-      maxlength: 280
+    photoText: {
+      type: //THIS WILL BE FOR THE PHOTO ,
     },
+
     createdAt: {
       type: Date,
       default: Date.now,
@@ -28,10 +26,10 @@ const thoughtSchema = new Schema(
   }
 );
 
-thoughtSchema.virtual('reactionCount').get(function() {
+photoSchema.virtual('reactionCount').get(function() {
   return this.reactions.length;
 });
 
-const Thought = model('Thought', thoughtSchema);
+const Photo = model('Photo', photoSchema);
 
-module.exports = Thought;
+module.exports = Photo;
--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,10 +1,10 @@
-const faker = require('faker');
+const faker = require("faker");
 
-const db = require('../config/connection');
-const { Thought, User } = require('../models');
+const db = require("../config/connection");
+const { Photo, User } = require("../models");
 
-db.once('open', async () => {
-  await Thought.deleteMany({});
+db.once("open", async () => {
+  await Photo.deleteMany({});
   await User.deleteMany({});
 
   // create user data
@@ -28,29 +28,31 @@ db.once('open', async () => {
     let friendId = userId;
 
     while (friendId === userId) {
-      const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+      const randomUserIndex = Math.floor(
+        Math.random() * createdUsers.ops.length
+      );
       friendId = createdUsers.ops[randomUserIndex];
     }
 
     await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
   }
 
-  // create thoughts
-  let createdThoughts = [];
+  // create photo
+  let createdPhotos = [];
   for (let i = 0; i < 100; i += 1) {
-    const thoughtText = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+    const photoText = faker.lorem.words(Math.round(Math.random() * 20) + 1);
 
     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
     const { username, _id: userId } = createdUsers.ops[randomUserIndex];
 
-    const createdThought = await Thought.create({ thoughtText, username });
+    const createdPhoto = await Photo.create({ photoText, username });
 
     const updatedUser = await User.updateOne(
       { _id: userId },
-      { $push: { thoughts: createdThought._id } }
+      { $push: { photos: createdPhoto._id } }
     );
 
-    createdThoughts.push(createdThought);
+    createdPhotos.push(createdPhoto);
   }
 
   // create reactions
@@ -60,16 +62,16 @@ db.once('open', async () => {
     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
     const { username } = createdUsers.ops[randomUserIndex];
 
-    const randomThoughtIndex = Math.floor(Math.random() * createdThoughts.length);
-    const { _id: thoughtId } = createdThoughts[randomThoughtIndex];
+    const randomPhotoIndex = Math.floor(Math.random() * createdPhotos.length);
+    const { _id: photoId } = createdPhotos[randomPhotoIndex];
 
-    await Thought.updateOne(
+    await Photo.updateOne(
       { _id: thoughtId },
       { $push: { reactions: { reactionBody, username } } },
       { runValidators: true }
     );
   }
 
-  console.log('all done!');
+  console.log("all done!");
   process.exit(0);
 });
